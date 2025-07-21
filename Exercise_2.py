d03b152c7cc1220d01df9d909ba5b58cd51df2a2
raj-softhealer <raj@softhealer.com>
2025-07-21 13:50:22 +0530

    python-ecercise-2

--- Exercise_2.py
+++ Exercise_2.py
@@ -119,9 +119,26 @@ d1 = {'a': 100, 'b': 200, 'c': 300}
 d2 = {'a': 300, 'b': 100, 'd': 400}
 
 
+d3={}
+
+for x,y in d1.items():
+    if x in d2.keys():
+        d3[x] = d1[x] + d2[x]
+    else:
+        d3[x] = d1[x]
+
+for x,y in d2.items():
+    if x not in d1.keys():
+        d3[x] = d2[x]
+        
+
+        
+
+print(d3)
 
 
 
+    
 
 
 
